4b331b5a98b938f155ea5398c43d6def91668325
KID <jyhsu2000@yahoo.com.tw>
2018-05-06 23:58:52 +0800

    Update navbar and links

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -7,9 +7,9 @@
         div.col
           //- Links
           div.list-group
-            router-link(:to="{name:'About'}").list-group-item.list-group-item-action 關於我們
-            router-link(:to="{name:'Activity'}").list-group-item.list-group-item-action 社團活動
-            router-link(:to="{name:'Achievement'}").list-group-item.list-group-item-action 成果發表
+            router-link.list-group-item.list-group-item-action(:to="{name:'ClubIntroduction'}") 關於我們
+            router-link.list-group-item.list-group-item-action(:to="{name:'Activity'}") 社團活動
+            router-link.list-group-item.list-group-item-action(:to="{name:'ActivityRecord'}") 成果發表
         div.col
         div.col
           <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHackerSir.tw%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=367162860292923" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
