566176a2c51fd7412e8a78b03eb82c1c3b2eed46
Chan, Danny <danny50610@yahoo.com.tw>
2018-05-08 00:18:31 +0800

    Auto switch heading class

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,8 @@
 <template lang="pug">
   div
     //- Heading
-    div.display-1 逢甲大學黑客社
+    h1.p-2.display-1.d-none.d-md-block 逢甲大學黑客社
+    h1.p-2.display-3.d-md-none 逢甲大學黑客社
     div.container
       div.row
         div.col
@@ -18,6 +19,9 @@
 
 <style scoped>
 .display-1 {
+  text-shadow: -2px 0 white, 0 2px white, 2px 0 white, 0 -2px white;
+}
+.display-3 {
   text-shadow: -2px 0 white, 0 2px white, 2px 0 white, 0 -2px white;
 }
 .list-group-item {
