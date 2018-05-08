47ea4f3d26974b536c0dc2211c89c9d342e23252
KID <jyhsu2000@yahoo.com.tw>
2018-05-08 16:00:54 +0800

    Install axios https://github.com/axios/axios

--- src/main.js
+++ src/main.js
@@ -4,6 +4,11 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 
+// axios. Usage: this.$http.get
+window.axios = require('axios')
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+Vue.prototype.$http = window.axios
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
