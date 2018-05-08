dc59f06734ce69a125460bc8fe88efdf34dab271
KID <jyhsu2000@yahoo.com.tw>
2018-05-08 18:19:56 +0800

    Auto get latest commit id using GitHub API

--- src/components/about/Rule.vue
+++ src/components/about/Rule.vue
@@ -2,7 +2,7 @@
   div
     h2 社團規章
     div
-      | （以下顯示版本：{{ ruleVersion }}，最新版本請至 #[a(href='https://github.com/HackerSir/Articles-of-association/blob/master/rules.md' target='_blank') rules.md] 查詢）
+      | （以下為當前最新版本：{{ ruleVersion | substring(0,8) }}，詳細變更紀錄請至 #[a(href='https://github.com/HackerSir/Articles-of-association' target='_blank') 這裡] 查詢）
     div.container
       div.card
         div.card-body.text-left(v-html='rule')
@@ -13,21 +13,28 @@ export default {
   data: function () {
     return {
       // 社規 commit 編號
-      ruleVersion: '4a57c208',
+      ruleVersion: '',
       rule: '<i class="fas fa-spinner fa-spin"></i> 載入中...'
     }
   },
   mounted () {
     let vm = this
-    // 利用 RawGit https://rawgit.com/ 作為 CDN
-    let ruleUrl = 'https://cdn.rawgit.com/HackerSir/Articles-of-association/' + this.ruleVersion + '/rules.md'
-    this.$http.get(ruleUrl)
+    // 由 GitHub 取得最新版本號
+    this.$http.get('https://api.github.com/repos/HackerSir/Articles-of-association/commits/master')
       .then(function (response) {
-        let showdown = require('showdown')
-        let converter = new showdown.Converter({
-          noHeaderId: true
-        })
-        vm.rule = converter.makeHtml(response.data)
+        vm.ruleVersion = response.data.sha
+        // 利用 RawGit https://rawgit.com/ 作為 CDN
+        let ruleUrl = 'https://cdn.rawgit.com/HackerSir/Articles-of-association/' + vm.ruleVersion + '/rules.md'
+        // 讀取 rules.md
+        vm.$http.get(ruleUrl)
+          .then(function (response) {
+            // 利用 showdown 解析 markdown
+            let showdown = require('showdown')
+            let converter = new showdown.Converter({
+              noHeaderId: true
+            })
+            vm.rule = converter.makeHtml(response.data)
+          })
       })
   }
 }
