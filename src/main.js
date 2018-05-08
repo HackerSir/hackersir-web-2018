18ea4f353a0ecf6b496cd6dd1806d38f5ff6343c
KID <jyhsu2000@yahoo.com.tw>
2018-05-08 18:02:54 +0800

    Install VueFilter https://github.com/wy-ei/vue-filter

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import VueFilter from 'vue-filter'
+Vue.use(VueFilter)
 
 // axios. Usage: this.$http.get
 window.axios = require('axios')
