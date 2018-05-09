05bb09b76f6b87d3335717fa31daab1f109171cb
KID <jyhsu2000@yahoo.com.tw>
2018-05-09 16:52:13 +0800

    Add links

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -5,14 +5,19 @@
     h1.p-2.display-3.d-md-none 逢甲大學黑客社
     div.container
       div.row
-        div.col-12.col-md
+        div.col-12.col-md-4
           //- Links
           div.list-group
             router-link.list-group-item.list-group-item-action(:to="{name:'ClubIntroduction'}") 關於我們
             router-link.list-group-item.list-group-item-action(:to="{name:'Activity'}") 社團活動
             router-link.list-group-item.list-group-item-action(:to="{name:'ActivityRecord'}") 成果發表
-        div.col-12.col-md
-        div.col-12.col-md
+          div.row.mb-2
+            div.col
+              a(href='https://www.facebook.com/groups/HackerSir/' target='_blank' title='FB公開社團') <i class="fab fa-facebook-square fa-3x fa-fw"></i>
+              a(href='https://github.com/HackerSir' target='_blank' title='GitHub') <i class="fab fa-github fa-3x fa-fw"></i>
+              a(href='https://gitlab.com/HackerSir' target='_blank' title='GitLab') <i class="fab fa-gitlab fa-3x fa-fw"></i>
+              a(href='https://www.youtube.com/channel/UCeDnuTpnq_4As-ceZiWsi4A' target='_blank' title='YouTube') <i class="fab fa-youtube fa-3x fa-fw"></i>
+        div.col-12.col-md-4.offset-md-4
           <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHackerSir.tw%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=367162860292923" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 
 </template>
