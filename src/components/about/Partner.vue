468f51ca53f46fdec8a5b81cf86a78cd7ecab16d
KID <jyhsu2000@yahoo.com.tw>
2018-05-26 10:31:29 +0800

    Extract partners data to partners.json

--- src/components/about/Partner.vue
+++ src/components/about/Partner.vue
@@ -5,37 +5,17 @@
       div.card
         div.card-body
           p(style={'font-size':'20px'}) 我們有許許多多的合作夥伴，他們來自世界各地，四面八方。感謝有他們，和我們並肩走來！
-      div.card
+      div.card(v-for="(partnersOfType, partnerType) in partners")
         div.card-body
-          h3 校內組織
-          div.d-md-flex
+          h3 {{ partnerType }}
+          div.d-md-flex(v-for="partner in partnersOfType")
             div
-              img.img-fluid(src='static/image/FISA.png')
+              img.img-fluid(:src='partner.image')
             div.text-left
-              h3.text-center.text-md-left FISA
-              h4.text-center.text-md-left 逢甲大學資訊安全策進會
-              p FISA資策會源自於遠古學長們的傳承，最初由李維斌老師所召集。
-              p 我們希望從學生時期就培養資安觀念，除了在校園中宣導基本的資安觀念，我們也提供想玩技術的夥伴們空間，讓更多關注資安的駭客們，有合法的環境，可以更深入的研究資安相關議題。
-      div.card
-        div.card-body
-          h3 社團
-          div.d-md-flex
-            div
-              img.img-fluid(src='static/image/NCHUIT.png')
-            div.text-left
-              h3.text-center.text-md-left NCHUIT
-              h4.text-center.text-md-left 中興大學資訊科學研習社
-              p 推廣興大的資訊風氣，使興大有更多能夠了解並且使用資訊科技的同學們，並且能夠透過社團互相交流、取暖(?)
-      div.card
-        div.card-body
-          h3 校外組織
-          div.d-md-flex
-            div
-              img.img-fluid(src='static/image/sitcon.png')
-            div.text-left
-              h3.text-center.text-md-left SITCON
-              h4.text-center.text-md-left 學生計算機年會
-              p SITCON 學生計算機年會，由對資訊抱持熱誠的學生們所組成的同名社群自發籌辦，希望能透過技術與知識的激盪，給予學生們一個用自身力量實踐夢想的舞台。本屆年會之主軸聚焦於真正的駭客精神，The True Hackers 。希望讓大家瞭解駭客精神的中心思想，以及駭客精神在各個領域的展現。
+              h3.text-center.text-md-left {{ partner.name }}
+              h4.text-center.text-md-left {{ partner.cht_name }}
+              p(v-for="descriptionLine in partner.description")
+                | {{ descriptionLine }}
 </template>
 
 <style scoped>
@@ -44,3 +24,18 @@ img {
   margin: 0.5rem;
 }
 </style>
+
+<script>
+export default {
+  data: function () {
+    return {
+      partners: {}
+    }
+  },
+  mounted () {
+    this.$http.get('static/data/partners.json').then(response => {
+      this.partners = response.data
+    })
+  }
+}
+</script>
