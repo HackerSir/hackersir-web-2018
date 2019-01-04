f7c4e3742838140f35c4612701f001d75a3eb6fe
KID <jyhsu2000@yahoo.com.tw>
2019-01-04 20:37:19 +0800

    Update color of link icons to wine red

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -13,10 +13,10 @@
             router-link.list-group-item.list-group-item-action(:to="{name:'ActivityRecord'}") 成果發表
           div.row.mb-2
             div.col
-              a(href='https://www.facebook.com/groups/HackerSir/' target='_blank' title='FB公開社團') #[i.fab.fa-facebook-square.fa-3x.fa-fw]
-              a(href='https://github.com/HackerSir' target='_blank' title='GitHub') #[i.fab.fa-github.fa-3x.fa-fw]
-              a(href='https://gitlab.com/HackerSir' target='_blank' title='GitLab') #[i.fab.fa-gitlab.fa-3x.fa-fw]
-              a(href='https://www.youtube.com/channel/UCeDnuTpnq_4As-ceZiWsi4A' target='_blank' title='YouTube') #[i.fab.fa-youtube.fa-3x.fa-fw]
+              a.link-icon(href='https://www.facebook.com/groups/HackerSir/' target='_blank' title='FB公開社團') #[i.fab.fa-facebook-square.fa-3x.fa-fw]
+              a.link-icon(href='https://github.com/HackerSir' target='_blank' title='GitHub') #[i.fab.fa-github.fa-3x.fa-fw]
+              a.link-icon(href='https://gitlab.com/HackerSir' target='_blank' title='GitLab') #[i.fab.fa-gitlab.fa-3x.fa-fw]
+              a.link-icon(href='https://www.youtube.com/channel/UCeDnuTpnq_4As-ceZiWsi4A' target='_blank' title='YouTube') #[i.fab.fa-youtube.fa-3x.fa-fw]
         div.col-12.col-md-4.offset-md-4
           <iframe src="https://www.facebook.com/plugins/page.php?href=https%3A%2F%2Fwww.facebook.com%2FHackerSir.tw%2F&tabs=timeline&width=340&height=500&small_header=false&adapt_container_width=true&hide_cover=false&show_facepile=true&appId=367162860292923" width="340" height="500" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowTransparency="true" allow="encrypted-media"></iframe>
 
@@ -31,5 +31,11 @@
 }
 .list-group-item {
   font-size: 2rem;
+}
+a.link-icon {
+  color: #9b0e27;
+}
+a.link-icon:hover {
+  color: #c60d25;
 }
 </style>
